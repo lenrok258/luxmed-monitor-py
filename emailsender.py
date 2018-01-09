f39c5d7c080923c0df38d07c98561d65a503940e
kornel.zemla <kornel.zemla@ocado.com>
2018-01-09 16:35:18 +0100

    Minor

--- emailsender.py
+++ emailsender.py
@@ -1,5 +1,5 @@
-import smtplib
 import json
+import smtplib
 
 with open('config.json') as data_file:
     config = json.load(data_file)
@@ -8,14 +8,13 @@ smtpUsername = config["email"]["smtpUsername"]
 smtpPassword = config["email"]["smtpPassword"]
 smtpUrl = config["email"]["smtpUrl"]
 sender = config["email"]["sender"]
-reciepent = config["email"]["reciepent"]
+recipient = config["email"]["reciepent"]
 
-server = smtplib.SMTP(smtpUrl, timeout=5)
-server.starttls()
-server.set_debuglevel(True)
-server.login(smtpUsername, smtpPassword)
 
-msg = "\nMessage test"
-
-server.sendmail(sender, reciepent, msg)
-server.quit()
+def send_email(message):
+    server = smtplib.SMTP(smtpUrl, timeout=5)
+    server.starttls()
+    server.set_debuglevel(True)
+    server.login(smtpUsername, smtpPassword)
+    server.sendmail(sender, recipient, message)
+    server.quit()
